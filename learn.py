7995605a70c9b6219eefe936d7ef5c8c1a8cd094
Samuel <dsh20462046@gmail.com>
2017-01-09 14:21:33 -0500

    Update learn.py

--- learn.py
+++ learn.py
@@ -51,3 +51,21 @@ fr = open('sample.txt', 'r')
 text = fr.read()
 print(text)
 fr.close()
+
+5. #下载csv文件，本地保存
+from urllib import request
+
+url = 'http://chart.finance.yahoo.com/table.csv?s=GOOG&a=11&b=9&c=2016&d=0&e=9&f=2017&g=d&ignore=.csv'
+
+def dl(csv_url):
+    response = request.urlopen(csv_url)
+    csv = response.read()
+    csv_str = str(csv)
+    lines = csv_str.split('\\n')
+    dest_url = r'goog.csv'
+    fx = open(dest_url, 'w')
+    for line in lines:
+        fx.write(line + '\n')
+    fx.close()
+
+dl(url)
