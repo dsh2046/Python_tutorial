96eda07332a9db3ce0c5b1ad021ea79ed9ca445f
Samuel <dsh20462046@gmail.com>
2017-01-11 10:12:44 -0500

    Update learn.py

--- learn.py
+++ learn.py
@@ -82,4 +82,44 @@ class abc(threading.Thread):
 th1 = abc(name='this is 1')               #结果：th1，th2同时进行
 th2 = abc(name='this is 2')
 th1.start()
-th2.start()                           
+th2.start()     
+
+7. #Python 程序： 提取网页高频词
+import requests
+from bs4 import BeautifulSoup
+import operator
+
+
+def start(url):
+    word_list = []
+    source_code = requests.get(url).text
+    soup = BeautifulSoup(source_code, 'lxml')
+    for post_text in soup.find_all('a', {'class': 'Fw(b)'}):
+        content = post_text.string
+        words = str(content).lower().split()
+        for each_word in words:
+            word_list.append(each_word)
+    clean_up_list(word_list)
+
+
+def clean_up_list(word_list):
+    clean_word_list = []
+    for word in word_list:
+        symbols = '!.@#$%^&*();:,~<>'
+        for i in range(0, len(symbols)):
+            word = word.replace(symbols[i], '')
+        if len(word) > 0:
+            clean_word_list.append(word)
+    create_dict(clean_word_list)
+
+def create_dict(clean_word_list):
+    word_count = {}
+    for word in clean_word_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    for k, v in sorted(word_count.items(), key=operator.itemgetter(1)):
+        print(k, v)
+
+start('https://ca.yahoo.com/')
