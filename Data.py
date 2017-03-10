904f2918537588fcb060ac56d57a15a02dd47156
Samuel <dsh20462046@gmail.com>
2017-03-10 13:21:57 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,1 +1,36 @@
+1.#读取csv
+import csv
+with open('stocks.csv') as f:
+    f_csv = csv.reader(f)
+   #f_csv = csv.Dictreader(f)   #可以使用列名去访问每一行的数据。比如，row['Symbol'] 或者 row['Change']
+    headers = next(f_csv)
+    for row in f_csv:
+        # Process row
+        
+2. #写入csv
+import csv
+headers = ['Symbol','Price','Date','Time','Change','Volume']
+rows = [('AA', 39.48, '6/11/2007', '9:36am', -0.18, 181800),
+         ('AIG', 71.38, '6/11/2007', '9:36am', -0.15, 195500),
+         ('AXP', 62.58, '6/11/2007', '9:36am', -0.46, 935000),
+       ]
 
+with open('stocks.csv','w') as f:
+    f_csv = csv.writer(f)
+    f_csv.writerow(headers)
+    f_csv.writerows(rows)
+    
+#如果你有一个字典序列的数据，可以像这样做：
+headers = ['Symbol', 'Price', 'Date', 'Time', 'Change', 'Volume']
+rows = [{'Symbol':'AA', 'Price':39.48, 'Date':'6/11/2007',
+        'Time':'9:36am', 'Change':-0.18, 'Volume':181800},
+        {'Symbol':'AIG', 'Price': 71.38, 'Date':'6/11/2007',
+        'Time':'9:36am', 'Change':-0.15, 'Volume': 195500},
+        {'Symbol':'AXP', 'Price': 62.58, 'Date':'6/11/2007',
+        'Time':'9:36am', 'Change':-0.46, 'Volume': 935000},
+        ]
+
+with open('stocks.csv','w') as f:
+    f_csv = csv.DictWriter(f, headers)
+    f_csv.writeheader()
+    f_csv.writerows(rows)
