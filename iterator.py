5e64949d491939f17dc844bd269ebd3ec0e981de
Samuel <dsh20462046@gmail.com>
2017-03-10 10:45:08 -0500

    Update iterator.py

--- iterator.py
+++ iterator.py
@@ -13,7 +13,7 @@ class Node:
     def add_child(self, node):
         self._children.append(node)
 
-    def __iter__(self):
+    def __iter__(self):                 #默认迭代函数
         return iter(self._children)
 
 # Example
@@ -26,3 +26,29 @@ if __name__ == '__main__':
 
     for ch in root:
         print(ch)
+# Node(1)
+# Node(2)
+
+2.#反向迭代
+class Countdown:
+    def __init__(self, start):
+        self.start = start
+
+    # Forward iterator
+    def __iter__(self):
+        n = self.start
+        while n > 0:
+            yield n
+            n -= 1
+
+    # Reverse iterator
+    def __reversed__(self):
+        n = 1
+        while n <= self.start:
+            yield n
+            n += 1
+
+for rr in reversed(Countdown(30)):
+    print(rr)
+for rr in Countdown(30):
+    print(rr)
