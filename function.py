c58a47c4004a1ac01bb75ab8cd2ef8109ccd71fc
Samuel <dsh20462046@gmail.com>
2017-03-10 17:18:08 -0500

    Update function.py

--- function.py
+++ function.py
@@ -26,3 +26,34 @@ def recv(maxsize, *, block):
 
 recv(1024, True) # TypeError
 recv(1024, block=True) # Ok
+
+4.Lambda
+
+l = []
+li = [lambda x,y=n:x+y for n in range(5)]
+for x in li:
+    l.append(x(2))
+
+print ','.join((str(x) for x in l))
+    
+#输出 2,3,4,5,6
+
+
+5.减少可调用对象的参数个数
+#比较两点距离并排序  #妙用Partial函数  # 利用 partial() 函数来固定某些参数值
+
+import math
+from functools import partial
+
+points = [ (1, 2), (3, 4), (5, 6), (7, 8) ]
+pt = (4, 3)
+def distance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return math.hypot(x2 - x1, y2 - y1)
+
+def dis(p):                          # 此处可用partial()代替
+    return distance(pt,p)            
+points.sort(key=dis)                 # points.sort(key=partial(distance, pt))
+print points                         # 结果相等
+# [(3, 4), (1, 2), (5, 6), (7, 8)]   # 另用lambda函数也可实现： points.sort(key=lambda x: distance(pt,x))
