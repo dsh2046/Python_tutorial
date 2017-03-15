e2aa18a04460d88c8b88da5276a3a5ba144c2e6b
Samuel <dsh20462046@gmail.com>
2017-03-15 16:56:36 -0400

    Update function.py

--- function.py
+++ function.py
@@ -77,3 +77,26 @@ def hello():
 
 print hello()
 #输出 <b><i>hello world</i></b>
+
+
+7.装饰器：计算函数运行时间
+
+import time
+from functools import wraps
+
+def timethis(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(end-start)
+        return result
+    return wrapper
+
+@timethis
+def abc(x, y):
+    z = x*x+y*y
+    return z
+
+print(abc(12,34))
