40537bd56ba349aa02ebe86013921316c2c4c5c1
Samuel <dsh20462046@gmail.com>
2017-03-16 15:12:26 -0400

    Update function.py

--- function.py
+++ function.py
@@ -100,3 +100,31 @@ def abc(x, y):
     return z
 
 print(abc(12,34))
+
+
+8.使用装饰器拓充类的功能
+def log_getattribute(cls):
+    orig_getattribute = cls.__getattribute__
+
+    def new_getattribute(self, name):
+        print('getting:', name)
+        return orig_getattribute(self, name)
+
+    cls.__getattribute__ = new_getattribute
+    return cls
+
+@log_getattribute
+class A:
+    def __init__(self,x):
+        self.x = x
+    def spam(self):
+        pass
+    
+a = A(42)
+print (a.x)
+print (a.spam())
+
+#getting: x
+#42
+#getting: spam
+#None
