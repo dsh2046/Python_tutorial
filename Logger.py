f21b8ffa9a7038fd28eef2225c9a24e576ab3d34
Samuel <dsh20462046@gmail.com>
2017-05-30 11:37:52 -0400

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -1,15 +1,15 @@
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)      # 设置默认等级（共５个等级: WARNING, INFO, DEBUG, CRITICAL, ERROR）
+logger.setLevel(logging.INFO)      ＃ 设置默认等级（共５个等级: WARNING, INFO, DEBUG, CRITICAL, ERROR）
 
-formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')  # 设置日志文件的显示格式
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')  ＃ 设置日志文件的显示格式
 
 file_handler = logging.FileHandler('sample.log')　　＃　输出到文件
 file_handler.setLevel(logging.ERROR)               ＃　设置日志文件等级
 file_handler.setFormatter(formatter)　　　　　　　　　＃　设置日志文件格式
 
-stream_handler = logging.StreamHandler()   # 输出到屏幕上显示
+stream_handler = logging.StreamHandler()   ＃ 输出到屏幕上显示
 stream_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
